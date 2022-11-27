79db45fd89f22cfe55544647d4f3aa46a404d447
Randy Birdsey <birdseyrk@gmail.com>
2022-11-27 11:53:24 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+
+    console.log('Recieved Get');
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
+
+    console.log('Recieved Post');
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(8085, () => {
+    console.log('Listening on 8085');
+})
