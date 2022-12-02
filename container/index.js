1c243c173acb9da136b9e70626760ed1bd18fd50
Randy Birdsey <birdseyrk@gmail.com>
2022-12-02 07:25:54 -0700

    message

--- container/index.js
+++ container/index.js
@@ -86,8 +86,9 @@ app.get('/uptime', (req, res) => {
 
     for (let myUptime in uptimes ) {
 
-        myUptimes[myUptime.hostname] = {myUptime};
-        console.log(' uptime [ hostname:' + myUptimes[myUptime].hostname + ' uptime:'+  myUptimes[myUptime].uptime + ' lastupdate:'+  myUptimes[myUptime].lastupdate +']'); ;
+        //myUptimes[myUptime.hostname] = {myUptime};
+        //console.log(' uptime [ hostname:' + myUptimes[myUptime].hostname + ' uptime:'+  myUptimes[myUptime].uptime + ' lastupdate:'+  myUptimes[myUptime].lastupdate +']'); ;
+        //console.log();
     }
 
     //res.send(hostname, uptime, lastupdate);
