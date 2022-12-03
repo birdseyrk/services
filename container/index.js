184a74def688541c832aa51566c815a9bd836a4b
Randy Birdsey <birdseyrk@gmail.com>
2022-12-03 06:03:45 -0700

    message

--- container/index.js
+++ container/index.js
@@ -6,6 +6,8 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 
+const epochNow = Math.round(Date.now() / 1000);
+
 const posts = {};
 
 myHosts = {};
@@ -29,6 +31,7 @@ testSleepES5();
 
 
 const uptimes = {};
+const hostStatus = {};
 const svrhostname = {};
 const svruptime = {};
 const svrlastupdate = {};
@@ -42,60 +45,47 @@ var exec = require('child_process').exec;
 app.get('/hoststatus', (req, res) => {
 
     console.log('Recieved Get hoststatus');
+    
+    for (let host in hostStatus ) {
+        myUptimes[host] = {"hostname":hostStatus[host].hostname, "uptime":hostStatus[host].uptime, "lastupdate":hostStatus[host].lastupdate, "epoch":hostStatus[host].epoch};
+        console.log("=======================================================================");
+        console.log(JSON.stringify(myUptimes));
+        //console.log(' uptime [ hostname:' + myUptimes[myUptime].hostname + ' uptime:'+  myUptimes[myUptime].uptime + ' lastupdate:'+  myUptimes[myUptime].lastupdate +']'); ;
+        //console.log(JSON.stringify(hostStatus[myUptime]));
+    }
+    console.log("Status myHosts " + JSON.stringify(myHosts));
 
-    exec("ls -la", (error, stdout, stderr) => {
-    //exec("hostname; /usr/bin/ansible-playbook /local/ansible/tests/webcalls/upTimeWebCall.yml", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
-
-    exec("hostname", (error, stdout, stderr) => {
-    //exec("hostname; /usr/bin/ansible-playbook /local/ansible/tests/webcalls/upTimeWebCall.yml", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
-
-    exec("which ansible-playbook", (error, stdout, stderr) => {
-    //exec("hostname; /usr/bin/ansible-playbook /local/ansible/tests/webcalls/upTimeWebCall.yml", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+    //res.send(hostname, uptime, lastupdate);
+    res.send(JSON.stringify(myUptimes));
 
     res.send(posts);
 });
 
 app.post('/hoststatus', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { hostname } = req.body;
 
     console.log('Recieved hoststatus'); 
+    const { hostname } = req.body;
+    const { uptime } = req.body;
+    const { lastupdate } = req.body;
+    const { epoch } = req.body;
+    const { meminfo } = req.body;
+    const { diskinfo } = req.body;
+    const { cpuinfo } = req.body;
+    const { processinfo } = req.body;
 
-    posts[hostname] = {
-        id, hostname
+    hostStatus[hostname] = {
+        hostname, uptime, lastupdate, epoch, meminfo, diskinfo, cpuinfo, processinfo
     };
 
-    res.status(201).send(posts[hostname]);
+    console.log("=======================================================================");
+    console.log('Hostname ===>' + JSON.stringify( hostStatus[hostname].hostname) +'<==='); 
+    console.log('uptime ===>' + JSON.stringify( hostStatus[hostname].uptime) +'<==='); 
+    console.log('lastupdate ===>' + JSON.stringify( hostStatus[hostname].lastupdate) +'<==='); 
+    console.log('epoch ===>' + JSON.stringify( hostStatus[hostname].epoch) +'<==='); 
+    
+    myStatus = {"hostname":hostStatus[hostname].hostname, "uptime":hostStatus[hostname].uptime, "lastupdate":hostStatus[hostname].lastupdate, "epoch":hostStatus[hostname].epoch};
+
+    res.status(201).send(myStatus);
 });
 
 
@@ -103,10 +93,9 @@ app.get('/uptime', (req, res) => {
 
     console.log('Recieved Get uptime');
     myUptimes = {};
-
+    
     for (let host in uptimes ) {
-
-        myUptimes[host] = {"hostname":uptimes[host].hostname, "uptime":uptimes[host].uptime, "lastupdate":uptimes[host].lastupdate};
+        myUptimes[host] = {"hostname":uptimes[host].hostname, "uptime":uptimes[host].uptime, "lastupdate":uptimes[host].lastupdate, "epoch":uptimes[host].epoch};
         console.log("=======================================================================");
         console.log(JSON.stringify(myUptimes));
         //console.log(' uptime [ hostname:' + myUptimes[myUptime].hostname + ' uptime:'+  myUptimes[myUptime].uptime + ' lastupdate:'+  myUptimes[myUptime].lastupdate +']'); ;
@@ -147,7 +136,7 @@ app.post('/uptime', (req, res) => {
     console.log('lastupdate ===>' + JSON.stringify( uptimes[hostname].lastupdate) +'<==='); 
     console.log('epoch ===>' + JSON.stringify( uptimes[hostname].epoch) +'<==='); 
     //uptimes[hostname]
-    myStatus = {"hostname":uptimes[hostname].hostname, "uptime":uptimes[hostname].uptime, "lastupdate":uptimes[hostname].lastupdate};
+    myStatus = {"hostname":uptimes[hostname].hostname, "uptime":uptimes[hostname].uptime, "lastupdate":uptimes[hostname].lastupdate, "epoch":uptimes[hostname].epoch};
         
     //res.status(201).send(uptimes[hostname]);
     res.status(201).send(myStatus);
