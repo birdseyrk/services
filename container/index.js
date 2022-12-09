527a7b4bba265f35a024f9cd792f5d4b072343d4
Randy Birdsey <birdseyrk@gmail.com>
2022-12-09 07:43:39 -0700

    message

--- container/index.js
+++ container/index.js
@@ -118,7 +118,7 @@ app.get('/uptime', (req, res) => {
     for (let host in  uptimes) {
         console.log(" adding " + host);
 
-        const myServer = new this.Server;
+        const myServer = Object.create(this.Server);
         myServer.hostname = uptimes[host].hostname;
         myServer.uptime = uptimes[host].uptime;
         myServer.lastupdate = uptimes[host].lastupdate;
