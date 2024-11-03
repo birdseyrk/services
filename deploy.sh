a093adfda2ab517d272bd49625ef37e9f7d250df
birdseyrk <birdseyrk@gmail.com>
2024-11-03 11:55:27 -0700

    Initial Delivery

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,6 @@ ansible-playbook -i /local/statusServices/ansible/inventory.txt /local/statusSer
 sleep 5;
 ansible-playbook -i /local/statusServices/ansible/inventory.txt /local/statusServices/ansible/buildandpushimage.yaml;
 sleep 5;
+/usr/bin/ansible-playbook -i /local/statusServices/ansible/inventory.txt /local/statusServices/ansible/delete-k8s-apps.yaml --extra-vars "myhost=k8s myfile=/local/statusServices/kubernetes/server-deployment.yaml"
+sleep 5;
 /usr/bin/ansible-playbook -i /local/statusServices/ansible/inventory.txt /local/statusServices/ansible/apply-k8s-apps.yaml --extra-vars "myhost=k8s myfile=/local/statusServices/kubernetes/server-deployment.yaml"
